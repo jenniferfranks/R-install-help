6246517ca1fe14afdce93add28345c7b1b988568
Jennifer Franks <franksjenniferm@gmail.com>
2024-09-04 15:19:27 -0700

    Update Re-InstallingPackages.R
    clean up with new posit instructions

--- Re-InstallingPackages.R
+++ Re-InstallingPackages.R
@@ -10,18 +10,38 @@
 #
 #      Recommend to run line-by-line to troubleshoot your specific install!
 # -----------------------------------------------------------------------------
+
+# Run if using R through terminal on HPC/Sasquatch
 chooseCRANmirror(ind=70)
 
+#----------
+# POSIT on Sasquatch INSTRUCTIONS: Activate the mamba environment using reticulate 
+#install.packages("reticulate")
+#library("reticulate")
+#use_condaenv("franks_lab")   #change to your environment name
+#-----------
+
+# More environment building for spatial analysis --- 
+# These first packages can be kind of problematic to install, they are needed for spatial analysis. 
+# SP, GEOS, GDAL are installed with the mamba environment, which is ok for terminal R but not POSIT. 
+
+
+#ok to skip if issues on terminal/HPC/Sasquatch
 install.packages("sp") 
+
 install.packages("rgeos", repos="http://R-Forge.R-project.org", type="source")
 require(rgeos)
 
-#install.packages("rgdal", repos="http://R-Forge.R-project.org", type="source") # deprecated as of May 2023?
-#require(rgdal)
+# ok to skip if issues on terminal/HPC/Sasquatch (deprecated as of May 2023?)
+install.packages("rgdal", repos="http://R-Forge.R-project.org", type="source") 
+require(rgdal)
 
 install.packages("remotes")
 library(remotes)
 install_github("r-spatial/sf")
+
+
+# Begin installing all other packages here ------------- 
 
 # list all packages installed on old version of R (MacOS location)
 if(exists("//Library/Frameworks/R.framework/Versions/4.0/Resources/library")){      #Intel 
@@ -31,18 +51,17 @@ if(exists("//Library/Frameworks/R.framework/Versions/4.0/Resources/library")){  
   temp <- list.dirs("~/Library/R/arm64/4.4/library/", 
                   full.names = F, recursive = F)
   } else {   # import list of packages from file 
-  temp <- read.table("list.of.R.packages.txt", header = F)
+  temp <- read.table("list.of.R.packages.txt", header = F)   #path to list of packages in a txt doc
   temp <- temp$V1
   }
   
 
-# install packages available from CRAN 
+# install CRAN packages ----
 new.packages <- temp[!(temp %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 
-# install packages from bioconductor 
-
+# install BIOCONDUCTOR packages----
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install(version = "3.19")
@@ -52,7 +71,7 @@ if(length(remaining.packages)) BiocManager::install(remaining.packages)
 BiocManager::install("Rgraphviz")
 
 
-# finish with installing Github directories
+# install GITHUB directories -----
 install.packages("devtools")
 
 # you may face issues with Github API limits. set a Personal Access Token to bypass this.
